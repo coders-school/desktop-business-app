dd93f3d4c09ee9f6a10122a24784920328e41568
Filip Tomaszewski <filip.tomaszewski@protonmail.com>
2024-03-22 23:13:01 +0100

    Introduce Patient class
    Issue: #38

---
+++ src/app/patient/patient.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "../person/person.hpp"
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+enum class Allergent
+{
+    Allergent = 0,
+    AnotherAllergent = 1,
+    SomeAllergent = 2,
+    DifferentAllergent = 3
+};
+
+class Patient : public Person
+{
+    static std::vector<std::shared_ptr<Patient>> patients_;
+
+    std::unordered_set<Allergent> allergents_;
+    size_t debt_;
+
+  public:
+    Patient() = delete;
+    Patient(const std::string name, const std::string last_name, const std::string pesel,
+            const std::unordered_set<Allergent> allergents);
+
+    static std::shared_ptr<Patient> getPatient(const std::string pesel);
+    static std::shared_ptr<Patient> getPatient(const std::string name, const std::string last_name);
+    static std::vector<std::shared_ptr<Patient>> getPatients();
+
+    std::string showInformationAboutPatient() const;
+    std::unordered_set<Allergent> getAllergents() const;
+    size_t getDebt() const;
+    void changePatientInformation();
+    void setDebt(const size_t debt);
+    void updateAllergents(const std::unordered_set<Allergent>& allergents);
+};
