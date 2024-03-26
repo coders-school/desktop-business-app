6c8c147118ab84a50a4f56539672dd302d70d469
Maciej Białczak <m.bialczak@protonmail.com>
2024-03-26 20:25:04 +0100

    Resolve remarks part 2

--- src/tests/ut/visit/visit_test.cpp
+++ src/tests/ut/visit/visit_test.cpp
@@ -8,31 +8,31 @@ namespace
 {
 TEST_F(VisitTestsFixture, ExtentDemo)
 {
-    const auto& content = Visit::getExtent().size();
-    const auto& expected = 3;
+    const auto content = Visit::getExtent().size();
+    const auto expected = 3;
     EXPECT_EQ(content, expected);
 }
 
 TEST_F(VisitTestsFixture, AssociationDemo)
 {
-    const auto& content = visit1->getDoctorAssociation()->getLastName();
-    const auto& expected = "Tracz";
+    const auto content = visit1->getDoctorAssociation()->getLastName();
+    const auto expected = "Tracz";
     EXPECT_EQ(content, expected);
 }
 
 TEST_F(VisitTestsFixture, AssociationDemo2)
 {
-    const auto& content = doc2->getVisitAssociations().begin()->get()->getVisitInformation();
-    const auto& expected = "Tworze klub ninja!";
+    const auto content = doc2->getVisitAssociations().begin()->get()->getVisitInformation();
+    const auto expected = "Tworze klub ninja!";
     EXPECT_EQ(content, expected);
 }
 
 TEST_F(VisitTestsFixture, AssociationDemo3)
 {
     std::string content;
-    const auto& associations = doc2->getVisitAssociations();
+    const auto associations = doc2->getVisitAssociations();
 
-    const auto& expected = "I w klubie sa sami fajni ludzie";
+    const auto expected = "I w klubie sa sami fajni ludzie";
 
     bool associationsContainExpectedVisitInformation = std::ranges::any_of(
         associations, [&expected](const auto& visitSPtr) { return visitSPtr->getVisitInformation() == expected; });
@@ -42,9 +42,9 @@ TEST_F(VisitTestsFixture, AssociationDemo3)
 
 TEST_F(VisitTestsFixture, UpdateTreatmentsShouldReplacePreviouslyStoredTreatments)
 {
-    const auto& visitOneTreatmentsBefore = visit1->getTreatments();
-    const auto& visitTwoTreatmentsBefore = visit2->getTreatments();
-    const auto& visitThreeTreatmentsBefore = visit3->getTreatments();
+    const auto visitOneTreatmentsBefore = visit1->getTreatments();
+    const auto visitTwoTreatmentsBefore = visit2->getTreatments();
+    const auto visitThreeTreatmentsBefore = visit3->getTreatments();
     ASSERT_TRUE(visitOneTreatmentsBefore.empty());
     ASSERT_EQ(visitTwoTreatmentsBefore, std::vector{Treatment::TeethWhitening});
     ASSERT_EQ(visitThreeTreatmentsBefore,
