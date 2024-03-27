e4bc1290c30e7f74c9caca8d765b09bd8029e3e7
Aleksiiej <aleksander.ramut@wp.pl>
2024-03-27 22:40:54 +0100

    Initializer list is being used now in constructor

--- src/app/equipment/equipment.cpp
+++ src/app/equipment/equipment.cpp
@@ -2,7 +2,7 @@
 
 Equipment::Equipment(const std::string &name, const double price, const int amount, const std::string &material,
                      const std::string &description)
-    : Product(name, price, amount), material_(material), description_(description)
+    : Product(name, price, amount), material_{material}, description_{description}
 {
 }
 
