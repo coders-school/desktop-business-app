17742563614ec525c00ac0ba191deb2f8b9c22fc
Filip Tomaszewski <filip.tomaszewski@protonmail.com>
2024-04-05 21:46:39 +0200

    Create getDoctor in context of Doctor
    Issue: #64

--- src/app/doctor/doctor.cpp
+++ src/app/doctor/doctor.cpp
@@ -28,3 +28,20 @@ void Doctor::createDoctor(const std::string& name, const std::string& surname, c
     Doctor doctor{name, surname, pesel};
     Clinic::appendDoctor(std::make_shared<Doctor>(doctor));
 }
+
+std::shared_ptr<Doctor>& Doctor::getDoctor(const std::string& name, const std::string& surname)
+{
+    auto doctors = Clinic::getDoctors();
+    auto exact_doctor = std::find_if(doctors.begin(), doctors.end(), [&](const std::shared_ptr<Doctor>& doctor) {
+        return (doctor->getName() == name) && (doctor->getSurname() == surname);
+    });
+
+    if (exact_doctor != doctors.end())
+    {
+        return *exact_doctor;
+    }
+    else
+    {
+        throw std::runtime_error("Doctor not found.");
+    }
+}
