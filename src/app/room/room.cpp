3d2b841fa208e8f9ec4603ca330362c5ebe1a7ff
Aleksiiej <aleksander.ramut@wp.pl>
2024-04-15 21:04:56 +0200

    Room introduced

---
+++ src/app/room/room.cpp
@@ -0,0 +1,32 @@
+#include "room.hpp"
+
+Room::Room(const uint roomNumber, const std::vector<Treatment>& treatments, std::shared_ptr<Warehouse>& ptrToWarehouse)
+    : roomNumber_{roomNumber}, treatments_{treatments}, ptrToWarehouse_{ptrToWarehouse}
+{
+}
+
+uint Room::getRoomNumber() const
+
+{
+    return roomNumber_;
+}
+
+std::vector<Treatment> Room::getTreatments() const
+{
+    return treatments_;
+}
+
+std::shared_ptr<Warehouse> Room::getWarehouse() const
+{
+    return ptrToWarehouse_;
+}
+
+void Room::setRoomAvalaibility(bool avalaible)
+{
+    avalaible_ = avalaible;
+}
+
+void Room::setWarehouse(std::shared_ptr<Warehouse>& ptrToWarehouse)
+{
+    ptrToWarehouse_ = ptrToWarehouse;
+}
