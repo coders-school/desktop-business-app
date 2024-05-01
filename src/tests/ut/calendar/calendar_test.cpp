1cc70f5d63eb0ddb6bfd9fbc721e496691fca7d6
Marcin <marcinjagielak90@gmail.com>
2024-05-01 16:05:40 +0200

    Added calendar structure

---
+++ src/tests/ut/calendar/calendar_test.cpp
@@ -0,0 +1,1 @@
+#include "calendar_test.hpp"
---
+++ src/tests/ut/calendar/calendar_test.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "calendar.hpp"
+#include "test_utils.hpp"
+#include "gtest/gtest.h"
+
+class CalendarTestFixture : public ::testing::Test{
+    
+};
