28176424b85b3582f84ade65def0a0aa6515cec8
Marcin <marcinjagielak90@gmail.com>
2024-05-01 19:21:50 +0200

    Connected calendar to extent

--- src/app/calendar/calendar.cpp
+++ src/app/calendar/calendar.cpp
@@ -1,1 +1,14 @@
 #include "calendar.hpp"
+#include "../clinic/clinic_facade.hpp"
+#include <iostream>
+
+void Calendar::createCalendar(){
+    if(!Clinic::getCalendar()){
+        Clinic::appendCalendar(std::make_shared<Calendar>());
+    }else{
+        throw std::length_error("There cannot be more than one calendar");
+    }
+}
+std::shared_ptr<Connector> Calendar::addConnector(std::chrono::year_month_day ymd, std::shared_ptr<Room>, std::shared_ptr<Doctor>){
+    return nullptr;
+}
--- src/app/calendar/calendar.hpp
+++ src/app/calendar/calendar.hpp
@@ -1,5 +1,17 @@
 #pragma once
+#include "connector.hpp"
+#include "../room/room.hpp"
+#include <chrono>
+#include <map>
+#include <memory>
+#include "../doctor/doctor.hpp"
+#include <stdexcept>
 
-class Calendar{
+class Calendar
+{
+    std::multimap<std::chrono::year_month_day, std::shared_ptr<Connector>> mapCalendar_;
 
+public:
+    static void createCalendar();
+    std::shared_ptr<Connector> addConnector(std::chrono::year_month_day ymd, std::shared_ptr<Room>, std::shared_ptr<Doctor>);
 };
