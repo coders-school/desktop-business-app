a18dcfd2b2b2aaa358535181b5cdcbdbe84c30e2
Marcin <marcinjagielak90@gmail.com>
2024-05-12 00:07:15 +0200

    medicine_test fixes

--- src/tests/ut/medicine/medicine_test.hpp
+++ src/tests/ut/medicine/medicine_test.hpp
@@ -2,8 +2,22 @@
 #include "product.hpp"
 #include "gtest/gtest.h"
 
+const std::string name{"anaesthetic"};
+constexpr int price{45};
+constexpr int amount{100};
+constexpr int expiration_date{10122025};
+constexpr std::pair storage_temperature{-10, 0};
+
 struct MedicineTest : ::testing::Test
 {
-    std::shared_ptr<Product> anaesthetic = std::make_shared<Medicine>(
-        std::string{"anaesthetic"}, 45, 100, 10122025, std::pair{-10, 0}, std::vector<std::string>{{"chemicals"}});
+    std::shared_ptr<Product> anaesthetic;
+
+  protected:
+    void SetUp()
+    {
+        const std::string name{"anaesthetic"};
+        const std::vector<std::string> composition{"chemicals"};
+        anaesthetic =
+            std::make_shared<Medicine>(name, price, amount, expiration_date, storage_temperature, composition);
+    }
 };
