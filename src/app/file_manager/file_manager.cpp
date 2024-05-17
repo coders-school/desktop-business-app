f992871405868389bf2972d137cd46488339cbf9
Boski_Janusz <patmich@onet.eu>
2024-05-17 17:14:14 +0200

    1. File paths.hpp helps to organize paths in project.
    2. Class file_manager provides making files.
       I   - It's using filesystem library to manipulate directories.
       II  - Class has three open modes. It's for read, save new and overwrite.
       III - files in protobuff should have .proto extension.
       IV  - manager throws error if files doesn't exist.
       V   - stream file is returned by reference.
    
    Using File manager + paths below:
    
        FileManager file_manager(PATH::WORKERS, "workers.proto",
                    FileManager::FileMode::OutputAppend);
    
        auto& output_file = file_manager.getFileRef();
    
    3. Folder database contains folder files.

---
+++ src/app/file_manager/file_manager.cpp
@@ -0,0 +1,45 @@
+#include "file_manager.hpp"
+#include <stdexcept>
+#include <iostream>
+
+FileManager::FileManager(const std::filesystem::path& path, const std::string& file_name, FileMode mode)
+    : file_name_(file_name), 
+      file_path_(std::filesystem::absolute(std::filesystem::current_path().parent_path() / path / file_name))
+{
+    switch (mode)
+    {
+    case FileMode::Input:
+        file_.open(file_path_, std::ios::in);
+        break;
+
+    case FileMode::OutputAppend:
+        file_.open(file_path_, std::ios::out | std::ios::app);
+        break;
+
+    case FileMode::OutputTruncate:
+        file_.open(file_path_, std::ios::out | std::ios::trunc);
+        break;
+
+    default:
+        throw std::invalid_argument{"Invalid file mode"};
+    }
+    if (!file_.is_open())
+    {
+        throw std::runtime_error{"Cannot open file"};
+    }
+}
+
+FileManager::~FileManager()
+{
+    file_.close();
+}
+
+std::fstream& FileManager::getFileRef()
+{
+    return file_;
+}
+
+std::string FileManager::getFilePath() const
+{
+    return file_path_;
+}
