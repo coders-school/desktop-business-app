92e340dd1816084b624b68d9c96d5632e0cd98a7
Boski_Janusz <patmich@onet.eu>
2024-05-18 17:33:07 +0200

    1. Two new functions to print content of specific file.
    2. Methods of class DataFileManager have as arguments path and file name.

--- src/app/data_file_manager/data_file_manager.hpp
+++ src/app/data_file_manager/data_file_manager.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <memory>
 #include <fstream>
+#include <filesystem>
 #include "../proto/clinic.pb.h"
 #include "doctor.hpp"
 #include "file_manager.hpp"
@@ -14,11 +15,13 @@ private:
     static void promptDoctor(clinic_data::Workers::Doctor* new_doctor, const std::shared_ptr<Doctor>& doctor_data);
     static void promptReceptionist(clinic_data::Workers::Receptionist* new_receptionist, const std::shared_ptr<Receptionist>& receptionist_data);
 public:
-    static void addDoctorToDatabase(const std::shared_ptr<Doctor>& new_doctor);
-    static void addReceptionistToDatabase(const std::shared_ptr<Receptionist>& new_receptionist);
-    static void removeDoctorFromFile(const std::shared_ptr<Doctor>& doctor);
-    static void removeReceptionistFromFile(const std::shared_ptr<Receptionist>& receptionist);
+    static void addDoctorToDatabase(const std::filesystem::path& path, const std::string& file_name, const std::shared_ptr<Doctor>& new_doctor);
+    static void addReceptionistToDatabase(const std::filesystem::path& path, const std::string& file_name, const std::shared_ptr<Receptionist>& new_receptionist);
+    static void removeDoctorFromFile(const std::filesystem::path& path, const std::string& file_name, const std::shared_ptr<Doctor>& doctor);
+    static void removeReceptionistFromFile(const std::filesystem::path& path, const std::string& file_name, const std::shared_ptr<Receptionist>& receptionist);
     static void loadDataForDoctors();
     static void loadDataForReceptionists();
     static void loadAllDataForWorkers();
+    static std::string printDoctors(const std::filesystem::path& path, const std::string& file_name);
+    static std::string printReceptionists(const std::filesystem::path& path, const std::string& file_name);
 };
