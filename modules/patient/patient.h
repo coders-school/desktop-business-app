f47260262b73d0c7e3dbd5fb72dd705813d9cffb
Filip Tomaszewski <filip.tomaszewski@protonmail.com>
2024-05-25 00:40:51 +0200

    Init Patient module

---
+++ modules/patient/patient.h
@@ -0,0 +1,126 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <vector>
+// all enum types should have following functions
+// getX returning std::string
+enum class Gender : std::uint8_t
+{
+    Male = 0,
+    Female = 1,
+    Unknown = 255
+};
+
+struct Name
+{
+    std::string first_name_;
+    std::string second_name_;
+    std::string last_name_;
+};
+
+struct Address
+{
+    std::string city_;
+    std::string country_;
+    std::string province_;
+    std::string street_;
+    std::string zip_code_;
+};
+
+class Pesel
+{
+    std::string pesel_;
+    std::string validatePesel(const std::string& pesel)
+    {
+        // move validation
+        return pesel;
+    }
+
+  public:
+    Pesel(const std::string& pesel) : pesel_{validatePesel(pesel)}
+    {
+    }
+
+    std::string getPesel()
+    {
+        return pesel_;
+    }
+};
+
+struct PhoneNumber
+{
+    std::string phone_code_; // +1, +48 (+ == 00)
+    std::string cellphone_number_;
+    std::string home_number;
+};
+
+class Person
+{
+    Name name_;
+    Address address_;
+    Pesel pesel_;
+    PhoneNumber phone_number_;
+
+  public:
+    Person(const Name& name, const Address& address, const Pesel& pesel, const PhoneNumber& phone_number)
+        : name_{name}, address_{address}, pesel_{pesel}, phone_number_{phone_number}
+    {
+    }
+};
+
+struct Payments
+{
+    std::uint8_t paid_;
+    std::uint8_t planned_;
+    std::uint8_t overdue_;
+    // paid -> total received from patient
+    // planned -> informative value for planned treatment
+    // overdue -> if patient did not have enough money,
+    // patient may pay in installments
+};
+
+enum class Allergen : std::uint8_t // to be updated
+{
+    Al = 0
+};
+
+class Allergens
+{
+    // this class should contain allergens container
+    // allow to add and remove allergen/s
+    // return list of allergens
+    // inform (true/false) if queried allergen is present
+    std::vector<Allergen> allergens_;
+
+  public:
+    // expected methods
+    // addAllergen(singular -> Allergen)
+    // addAllergen(plural -> set, vector?<Allergen>)
+    // checkAllergen(Allergen)
+};
+
+class Treatments
+{
+    // vector<treatment> done
+    // vector<treatment> planned
+  public:
+    // Treatments should contain logic
+    // planned -> set once visit is made
+    // done -> set once visit state switches
+};
+/**
+ * represents both personal and medical data of patient.
+ * It includes personal information such as the patients' name, address
+ * identification number and contact details. It manages financial
+ * data connected to particular patient. It contains finished 
+ * and planned treatment informations.
+ */
+struct Patient : public Person
+{
+    // asking myself if Patient should inherit from Person
+    // or if it should be a member
+    Payments payments_;
+    Allergens allergens_;
+    Treatments treatments_;
+};
