e878e28d0548cfba6e128968b6b0e14d5eec7210
Filip Tomaszewski <filip.tomaszewski@protonmail.com>
2024-05-25 01:06:08 +0200

    Redefine project rules Create example use of interfaces

--- modules/patient/patient.h
+++ modules/patient/patient.h
@@ -5,6 +5,28 @@
 #include <vector>
 // all enum types should have following functions
 // getX returning std::string
+
+// methods in a class should follow the following convention
+// GetData, CreateSometing, FindSomething
+
+// free functions should follow the following convention
+// getData, createSomething, findSomething
+
+// enum class have to have assigned values and inherit
+// types like std::uin8_t
+// Male = 0, Female = 1..
+
+// class, enum class, struct should follow the following naming conveniton
+// class Name, struct Name, enum class Name
+
+// interfaces naming should follow the following convention
+// class iName, struct iName
+
+// interfaces need a short description about the
+// context in which shall be used 
+
+// each interface -> class?? should have, if possible, a mock class
+// example will be shown 
 enum class Gender : std::uint8_t
 {
     Male = 0,
@@ -31,18 +53,18 @@ struct Address
 class Pesel
 {
     std::string pesel_;
-    std::string validatePesel(const std::string& pesel)
+    std::string ValidatePesel(const std::string& pesel)
     {
         // move validation
         return pesel;
     }
 
   public:
-    Pesel(const std::string& pesel) : pesel_{validatePesel(pesel)}
+    Pesel(const std::string& pesel) : pesel_{ValidatePesel(pesel)}
     {
     }
 
-    std::string getPesel()
+    std::string GetPesel()
     {
         return pesel_;
     }
@@ -109,18 +131,47 @@ class Treatments
     // planned -> set once visit is made
     // done -> set once visit state switches
 };
+
+// find better name for the interface
+class iVisitPatient
+{
+    // this interface shall be used within Visit context
+    // it should provide methods that will allow to:
+    // 1. set planned treatment
+    // 2. update the treatment state (planned -> done) once Visit is fulfiled
+    // 3. other.. 
+  public:
+    virtual void SetTreatment() = 0;
+};
+
+// find better name for the interface
+class iSerdePatient
+{
+    public:
+    virtual void SerializeData() = 0;
+
+};
+
 /**
  * represents both personal and medical data of patient.
  * It includes personal information such as the patients' name, address
  * identification number and contact details. It manages financial
- * data connected to particular patient. It contains finished 
+ * data connected to particular patient. It contains finished
  * and planned treatment informations.
  */
-struct Patient : public Person
+class Patient : public iVisitPatient, public iSerdePatient
 {
-    // asking myself if Patient should inherit from Person
-    // or if it should be a member
+    Person person_;
     Payments payments_;
     Allergens allergens_;
     Treatments treatments_;
+
+  public:
+    void SetTreatment() override
+    {
+    }
+
+    void SerializeData() override
+    {
+    }
 };
