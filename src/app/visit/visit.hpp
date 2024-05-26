053d5efc8bb4a7413c8c44f57d3f97a440546b0a
Aleksiiej <aleksander.ramut@wp.pl>
2024-05-26 19:44:27 +0200

    ammendments

--- src/app/visit/visit.hpp
+++ src/app/visit/visit.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include "status.hpp"
+#include "visitStatus.hpp"
 #include "treatment.hpp"
 #include <memory>
 #include <set>
@@ -16,7 +16,7 @@ class Visit : public std::enable_shared_from_this<Visit>
     std::weak_ptr<Patient> patient_;
     std::weak_ptr<Room> room_;
     std::vector<Treatment> treatments_;
-    Status status_;
+    VisitStatus visit_status_;
     std::string visit_information_;
 
     Visit(const std::shared_ptr<Doctor>& doctor, const std::vector<Treatment>& treatments = {});
@@ -26,14 +26,14 @@ class Visit : public std::enable_shared_from_this<Visit>
     std::weak_ptr<Patient> getPatient() const;
     std::weak_ptr<Room> getRoom() const;
     std::vector<Treatment> getTreatments() const;
-    Status getstatus() const;
+    VisitStatus getStatus() const;
     std::string getVisitInformation() const;
 
     void setPatient(const std::shared_ptr<Patient>& patient);
     void setRoom(const std::shared_ptr<Room>& room);
     void setVisitInformation(const std::string& visit_information);
     void updateTreatments(const std::vector<Treatment>& new_treatments);
-    void setStatus(Status status);
+    void setStatus(VisitStatus visit_status);
 
     static void createVisit(const std::shared_ptr<Doctor>& doctor);
 };
