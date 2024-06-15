7a83eb02d2a70cb882f93fe7a9a40032a49f0433
Filip Tomaszewski <filip.tomaszewski@protonmail.com>
2024-06-16 00:30:50 +0200

    Remove serde interface and add protos

--- modules/backend/clinic/staff/doctor/Doctor.cpp
+++ modules/backend/clinic/staff/doctor/Doctor.cpp
@@ -22,16 +22,6 @@ common::specialization::Specialization Doctor::GetSpecialization() const
     return specialization_;
 }
 
-void Doctor::Serialize()
-{
-    // TODO Serialization logic
-}
-
-void Doctor::Deserialize()
-{
-    // TODO Deserialization logic
-}
-
 void Doctor::UpdateSpecialization(const Specialization specialization)
 {
     specialization_ = specialization;
