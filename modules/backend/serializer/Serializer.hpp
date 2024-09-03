4935dbc59c522a4c1ff3a38f90bf041d05f037eb
Filip Tomaszewski <filip.tomaszewski@protonmail.com>
2024-09-04 00:41:44 +0200

    Add file manager and staff deserialization

--- modules/backend/serializer/Serializer.hpp
+++ modules/backend/serializer/Serializer.hpp
@@ -1,6 +1,23 @@
 #pragma once
 
+#include "SerializerHelper.hpp"
+#include "clinic/staff/Staff.hpp"
+#include "common/file_manager/FileManager.hpp"
+#include "common/file_manager/Paths.hpp"
+
 namespace serde
 {
+
+void loadStaffData()
+{
+    FileManager file_manager(path::DEFAULT, "staff.proto", FileManager::FileMode::Input);
+    auto& input_file = file_manager.getFileRef();
+    proto_staff::Staff proto_staff{};
+    proto_staff.ParseFromIstream(&input_file);
+    clinic::staff::Staff staff{};
+    deserializeStaff(staff, proto_staff);
+
+    google::protobuf::ShutdownProtobufLibrary();
+}
 
 } // namespace serde
--- modules/backend/serializer/SerializerHelper.hpp
+++ modules/backend/serializer/SerializerHelper.hpp
@@ -22,13 +22,33 @@ namespace serde
     return proto_specialization;
 }
 
-void serializeAddress(::proto_common::PersonalData_Address* address, const ::common::PersonalData& personal_data)
+::common::specialization::Specialization deserializeSpecialization(const ::proto_common::Specialization specialization)
 {
-    address->set_city(personal_data.getAddress().city_);
-    address->set_country(personal_data.getAddress().country_);
-    address->set_province(personal_data.getAddress().province_);
-    address->set_street(personal_data.getAddress().street_);
-    address->set_zip_code(personal_data.getAddress().zip_code_);
+    ::common::specialization::Specialization specialization_{::common::specialization::Specialization::Unknown};
+    switch (specialization)
+    {
+    case ::proto_common::Specialization::DENTIST:
+        specialization_ = ::common::specialization::Specialization::Dentist;
+        break;
+    default:
+        break;
+    }
+    return specialization_;
+}
+
+void serializeAddress(::proto_common::PersonalData_Address* proto_address, const ::common::Address& address)
+{
+    proto_address->set_city(address.city_);
+    proto_address->set_country(address.country_);
+    proto_address->set_province(address.province_);
+    proto_address->set_street(address.street_);
+    proto_address->set_zip_code(address.zip_code_);
+}
+
+::common::Address deserializeAddress(const ::proto_common::PersonalData_Address& proto_address)
+{
+    return {proto_address.city(), proto_address.country(), proto_address.province(), proto_address.street(),
+            proto_address.zip_code()};
 }
 
 void serializePhoneNumber(::proto_common::PersonalData_PhoneNumber* phone_number,
@@ -40,6 +60,12 @@ void serializePhoneNumber(::proto_common::PersonalData_PhoneNumber* phone_number
     phone_number->set_home_number_(phone_number_.home_number_);
 }
 
+::common::PhoneNumber deserializePhoneNumber(const ::proto_common::PersonalData_PhoneNumber& proto_phone_number)
+{
+    return {proto_phone_number.cellphone_code_(), proto_phone_number.cellphone_number_(),
+            proto_phone_number.home_number_code_(), proto_phone_number.home_number_()};
+}
+
 void serializeName(::proto_common::PersonalData_Name* name, const ::common::Name& name_)
 {
     name->set_first_name(name_.first_name_);
@@ -47,18 +73,36 @@ void serializeName(::proto_common::PersonalData_Name* name, const ::common::Name
     name->set_last_name(name_.last_name_);
 }
 
+::common::Name deserializeName(const ::proto_common::PersonalData_Name& proto_name)
+{
+    return {proto_name.first_name(), proto_name.second_name(), proto_name.last_name()};
+}
+
 void serializePesel(::proto_common::PersonalData_Pesel* pesel, const ::common::Pesel& pesel_)
 {
     pesel->set_pesel(pesel_.getPesel());
+}
+
+::common::Pesel deserializePesel(const ::proto_common::PersonalData_Pesel& proto_pesel)
+{
+    return {proto_pesel.pesel()};
 }
 
 void serializePersonalData(::proto_common::PersonalData* proto_personal_data,
                            const ::common::PersonalData& personal_data)
 {
     serializeName(proto_personal_data->mutable_name(), personal_data.getName());
-    serializeAddress(proto_personal_data->mutable_address(), personal_data);
+    serializeAddress(proto_personal_data->mutable_address(), personal_data.getAddress());
     serializePhoneNumber(proto_personal_data->mutable_phone_numbers(), personal_data.getPhoneNumber());
     serializePesel(proto_personal_data->mutable_pesel(), personal_data.getPesel());
+}
+
+::common::PersonalData deserializePersonalData(const ::proto_common::PersonalData& proto_personal_data)
+{
+    ::common::PersonalData personal_data{
+        deserializeName(proto_personal_data.name()), deserializeAddress(proto_personal_data.address()),
+        deserializePhoneNumber(proto_personal_data.phone_numbers()), deserializePesel(proto_personal_data.pesel())};
+    return personal_data;
 }
 
 void serializeDoctor(const ::clinic::staff::doctor::Doctor& doctor, ::proto_staff::Doctor* proto_doctor)
@@ -67,11 +111,26 @@ void serializeDoctor(const ::clinic::staff::doctor::Doctor& doctor, ::proto_staf
     proto_doctor->set_specialization(serializeSpecialization(doctor.GetSpecialization()));
 }
 
+::clinic::staff::doctor::Doctor deserializeDoctor(const ::proto_staff::Doctor& proto_doctor)
+{
+    return {deserializePersonalData(proto_doctor.personaldata()),
+            deserializeSpecialization(proto_doctor.specialization())};
+}
+
 void serializeStaff(const ::clinic::staff::Staff& staff, ::proto_staff::Staff* proto_staff)
 {
     for (const auto& doctor : staff.getDoctors())
     {
         serializeDoctor(doctor, proto_staff->add_doctor());
+    }
+}
+
+void deserializeStaff(::clinic::staff::Staff& staff, const ::proto_staff::Staff& proto_staff)
+{
+    for (const auto& proto_doctor : proto_staff.doctor())
+    {
+        auto doctor = deserializeDoctor(proto_doctor);
+        staff.addDoctor(doctor);
     }
 }
 
