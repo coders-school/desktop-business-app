427c99ef984c6dcb8975fba759a13639f3307847
Aleksiiej <aleksander.ramut@wp.pl>
2024-10-11 04:46:20 +0200

    Widgets changed to Qml and that works, also in container

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 apt update 
-apt install -y libgtest-dev libgmock-dev cmake make g++ 
+apt install -y libgtest-dev libgmock-dev cmake make g++
 apt install -y qt6-base-dev
+apt install -y qt6-declarative-dev
 apt install -y libgl1-mesa-dev  
 apt install -y libssl-dev 
 apt install -y nlohmann-json3-dev
@@ -9,3 +10,4 @@ apt install -y libgoogle-glog-dev
 apt install -y protobuf-compiler
 apt install -y libprotobuf-dev
 apt install -y git
+apt install -y qml6-module-*
