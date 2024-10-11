f6203913a58d49825cd7af3e9f3b4992e2cd6546
Aleksiiej <aleksander.ramut@wp.pl>
2024-10-11 03:40:32 +0200

    ctest works again

--- tests_and_backend_clean_build.sh
+++ tests_and_backend_clean_build.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
+# Exit script on first error
+set -e
+
+# Check if the external_libraries directory does not exist
+if [ ! -d "external_libraries" ]; then
+  # If it does not exist, create it
+  mkdir external_libraries
+fi
+
+# Check if the libs directory does not exist
+if [ ! -d "libs" ]; then
+  # If it does not exist, create it
+  mkdir libs
+fi
+
 # This script is used to clean the build directory then build the backend and run tests
 rm -rf build
-
-set -e
 
 mkdir build && cd build
 
 cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=1 ..
 
 cmake --build . --parallel --clean-first -- -j2
+
+ctest --output-on-failure
