9d3f2c249feaa52f928b8e85e49bea1fe55d96e8
Aleksiiej <aleksander.ramut@wp.pl>
2024-10-11 03:41:44 +0200

    duplication in dependancy.sh removed

--- dependencies.sh
+++ dependencies.sh
@@ -2,7 +2,6 @@
 apt update 
 apt install -y libgtest-dev libgmock-dev cmake make g++ 
 apt install -y qt6-base-dev
-apt install -y qt6-base-dev
 apt install -y libgl1-mesa-dev  
 apt install -y libssl-dev 
 apt install -y nlohmann-json3-dev
