312943e93ee550632dfff41d7cabab2647036cc4
Boski_Janusz <patmich@onet.eu>
2024-10-21 22:42:12 +0200

    clang format

--- modules/backend/clinic/staff/Staff.hpp
+++ modules/backend/clinic/staff/Staff.hpp
@@ -8,29 +8,29 @@ namespace clinic
 {
 namespace staff
 {
-    using DoctorPtr = doctor::DoctorPtr;
-    using iDoctor = doctor::iDoctor;
-    using ReceptionistPtr = receptionist::ReceptionistPtr;
-    using iReceptionist = receptionist::iReceptionist;
+using DoctorPtr = doctor::DoctorPtr;
+using iDoctor = doctor::iDoctor;
+using ReceptionistPtr = receptionist::ReceptionistPtr;
+using iReceptionist = receptionist::iReceptionist;
 
-    class Staff
-    {
-      public:
-        void addDoctor(DoctorPtr doctor);
-        iDoctor* getDoctor(const common::Name& name);
-        std::vector<iDoctor*> getDoctors() const;
+class Staff
+{
+  public:
+    void addDoctor(DoctorPtr doctor);
+    iDoctor* getDoctor(const common::Name& name);
+    std::vector<iDoctor*> getDoctors() const;
 
-        void addReceptionist(ReceptionistPtr receptionist);
-        iReceptionist* getReceptionist(const common::Name& name);
-        std::vector<iReceptionist*> getReceptionists() const;
+    void addReceptionist(ReceptionistPtr receptionist);
+    iReceptionist* getReceptionist(const common::Name& name);
+    std::vector<iReceptionist*> getReceptionists() const;
 
-      private:
-        template <typename Container>
-        typename Container::value_type::pointer getPersonByName(const Container& container, const common::Name& name) const;
+  private:
+    template <typename Container>
+    typename Container::value_type::pointer getPersonByName(const Container& container, const common::Name& name) const;
 
-        std::vector<DoctorPtr> doctors_;
-        std::vector<ReceptionistPtr> receptionists_;
-    };
+    std::vector<DoctorPtr> doctors_;
+    std::vector<ReceptionistPtr> receptionists_;
+};
 
 } // namespace staff
 } // namespace clinic
