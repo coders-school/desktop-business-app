6576800db053ba8ea2337da973a3c2f7b1d35d5b
fbtom <92322227+fbtom@users.noreply.github.com>
2024-10-27 11:01:44 +0100

    Create SerializerHelper Address tests (#175)
    * Create SerializerHelper AddressTests
    
    * Create SerializerHelper PhoneNumber tests

--- modules/backend/tests/unit/serializer/SerializerHelperTests.cpp
+++ modules/backend/tests/unit/serializer/SerializerHelperTests.cpp
@@ -18,4 +18,57 @@ TEST(SerializerHelperTests, GivenProtoSpecializationWhenDeserializeCalledExpectS
     EXPECT_EQ(specialization, ::common::specialization::Specialization::Dentist);
 }
 
+TEST(SerializerHelperTests, GivenAddressWhenSerializeCalledExpectProtoAddress)
+{
+    auto address = ::common::Address{"city", "country", "province", "street", "zip_code"};
+    auto proto_address = ::proto_common::PersonalData_Address{};
+    ::serde::serializeAddress(&proto_address, address);
+    EXPECT_EQ(proto_address.city(), "city");
+    EXPECT_EQ(proto_address.country(), "country");
+    EXPECT_EQ(proto_address.province(), "province");
+    EXPECT_EQ(proto_address.street(), "street");
+    EXPECT_EQ(proto_address.zip_code(), "zip_code");
+}
+
+TEST(SerializerHelperTests, GivenProtoAddressWhenDeserializeCalledExpectAddress)
+{
+    auto proto_address = ::proto_common::PersonalData_Address{};
+    proto_address.set_city("city");
+    proto_address.set_country("country");
+    proto_address.set_province("province");
+    proto_address.set_street("street");
+    proto_address.set_zip_code("zip_code");
+    auto address = ::serde::deserializeAddress(proto_address);
+    EXPECT_EQ(address.city_, "city");
+    EXPECT_EQ(address.country_, "country");
+    EXPECT_EQ(address.province_, "province");
+    EXPECT_EQ(address.street_, "street");
+    EXPECT_EQ(address.zip_code_, "zip_code");
+}
+
+TEST(SerializerHelperTests, GivenPhoneNumberWhenSerializeCalledExpectProtoPhoneNumber)
+{
+    auto phone_number = ::common::PhoneNumber{"cellphone_code", "cellphone_number", "home_number_code", "home_number"};
+    auto proto_phone_number = ::proto_common::PersonalData_PhoneNumber{};
+    ::serde::serializePhoneNumber(&proto_phone_number, phone_number);
+    EXPECT_EQ(proto_phone_number.cellphone_code_(), "cellphone_code");
+    EXPECT_EQ(proto_phone_number.cellphone_number_(), "cellphone_number");
+    EXPECT_EQ(proto_phone_number.home_number_code_(), "home_number_code");
+    EXPECT_EQ(proto_phone_number.home_number_(), "home_number");
+}
+
+TEST(SerializerHelperTests, GivenProtoPhoneNumberWhenDeserializeCalledExpectPhoneNumber)
+{
+    auto proto_phone_number = ::proto_common::PersonalData_PhoneNumber{};
+    proto_phone_number.set_cellphone_code_("cellphone_code");
+    proto_phone_number.set_cellphone_number_("cellphone_number");
+    proto_phone_number.set_home_number_code_("home_number_code");
+    proto_phone_number.set_home_number_("home_number");
+    auto phone_number = ::serde::deserializePhoneNumber(proto_phone_number);
+    EXPECT_EQ(phone_number.cellphone_code_, "cellphone_code");
+    EXPECT_EQ(phone_number.cellphone_number_, "cellphone_number");
+    EXPECT_EQ(phone_number.home_number_code_, "home_number_code");
+    EXPECT_EQ(phone_number.home_number_, "home_number");
+}
+
 } // namespace
